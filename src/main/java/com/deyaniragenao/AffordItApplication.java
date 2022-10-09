d7d73e6914afa204464d58aad21ec8c39affac1b
Deyanira Genao <deyagenao@gmail.com>
2022-10-09 12:54:03 -0400

    create initial file structure and set up application.properties

---
+++ src/main/java/com/deyaniragenao/AffordItApplication.java
@@ -0,0 +1,13 @@
+package com.deyaniragenao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffordItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffordItApplication.class, args);
+	}
+
+}
