d8e17e3d87a9e292019c9fd7fd322b0bae17879d
Deyanira Genao <deyagenao@gmail.com>
2022-10-19 17:09:16 -0400

    create tests for custom methods in repositories

--- src/main/java/com/deyaniragenao/repository/AccountRepository.java
+++ src/main/java/com/deyaniragenao/repository/AccountRepository.java
@@ -8,12 +8,5 @@ import com.deyaniragenao.model.Account;
 @RepositoryRestResource
 public interface AccountRepository extends JpaRepository<Account, String>{
 
-////	@Query("SELECT a FROM Account WHERE a.")
-////	@Query(value = "SELECT * FROM author WHERE id = ?1",
-////			nativeQuery = true) -- another acceptable format 
-//	@Query("SELECT u FROM User u JOIN FETCH u.accounts "
-////			+ "Account a "
-////			+ "JOIN users_accounts ua ON a.id = ua.acc_id "
-//			+ "WHERE u.id = ?1")
-//	Set<Account> findAccountsByUserId(String id);
+
 }
--- src/test/java/com/deyaniragenao/repository/RepositoryTest.java
+++ src/test/java/com/deyaniragenao/repository/RepositoryTest.java
@@ -1,30 +1,45 @@
 package com.deyaniragenao.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import com.deyaniragenao.model.Account;
 import com.deyaniragenao.model.Category;
+import com.deyaniragenao.model.Expense;
+import com.deyaniragenao.model.FinancialEntryFrequency;
+import com.deyaniragenao.model.Income;
+import com.deyaniragenao.model.User;
 
-//@SpringBootTest
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class RepositoryTest {
-
-	@Autowired 
-	TestEntityManager testEntityManager;
 	
 	@Autowired
 	CategoryRepository categoryRepository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	AccountRepository accountRepository;
+	@Autowired
+	ExpenseRepository expenseRepository;
+	@Autowired
+	IncomeRepository incomeRepository;
+	
+	private User user = new User("Andrea", "Herrera", "an@he.com", "1234");
+	private Account account = new Account("Main Account");
+	private Category categoryE = new Category("Rent", "expense");
+	private Category categoryI = new Category("Pay", "income");
 	
 	@ParameterizedTest
 	@ValueSource(strings = {"expense", "income"})
@@ -34,7 +49,53 @@ public class RepositoryTest {
 		assertTrue(categories.size() > 0);
 	}
 	
+	@Test
+	void findUserByEmailTest() {
+		User expectedUser = user;
+		userRepository.save(expectedUser);
+		User actualUser = userRepository.findByEmail(expectedUser.getEmail());
+		assertEquals(expectedUser.getEmail(), actualUser.getEmail());
+	}
 	
-
+	@Test 
+	void findExpenseTotalByAccountIdTest() {
+		userRepository.save(user);
+		accountRepository.save(account);
+		categoryRepository.save(categoryE);
+		Expense exp1 = new Expense(); 
+		exp1.setName("Expense 1");
+		exp1.setAmount(new BigDecimal("9.99"));
+		exp1.setFrequency(FinancialEntryFrequency.MONTHLY);
+		exp1.setMonthlyAmount(exp1.calculateMonthlyAmount());
+		exp1.setCategory(categoryE);
+		exp1.setUser(user);
+		exp1.setAccount(account);
+		expenseRepository.save(exp1);
+		
+		BigDecimal total = expenseRepository.findExpenseTotalByAccountId(account.getId());
+		assertTrue(total.equals(new BigDecimal("9.99")));
+		
+	}
+	
+	@Test
+	void findIncomeTotalByAccountIdTest() {
+		userRepository.save(user);
+		accountRepository.save(account);
+		categoryRepository.save(categoryI);
+		Income in1 = new Income(); 
+		in1.setName("Income 1");
+		in1.setAmount(new BigDecimal("9.99"));
+		in1.setFrequency(FinancialEntryFrequency.MONTHLY);
+		in1.setMonthlyAmount(in1.calculateMonthlyAmount());
+		in1.setCategory(categoryI);
+		in1.setUser(user);
+		in1.setAccount(account);
+		incomeRepository.save(in1);
+		
+		BigDecimal total = incomeRepository.findIncomeTotalByAccountId(account.getId());
+		assertTrue(total.equals(new BigDecimal("9.99")));
+	}
+	
+	
 	
 }
